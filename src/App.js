8be2030d6107f47c52f2512bd89774e18b9db890
drrh12 <flavioandrade0201@gmail.com>
2021-05-01 21:57:37 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Flex01 from "./components/flex-design-01/Flex01";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex01></Flex01>
     </div>
   );
 }
